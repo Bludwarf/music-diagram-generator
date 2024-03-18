b45f3ecad34ff90ec6179710e2f67d15d7a56c3e
Bludwarf <bludwarf@gmail.com>
2024-03-18 12:28:20 +0100

    Correction autres tests #9

--- src/app/song/song.component.spec.ts
+++ src/app/song/song.component.spec.ts
@@ -1,17 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SongComponent } from './song.component';
+import {ActivatedRoute} from "@angular/router";
+import SpyObj = jasmine.SpyObj;
+import {injectSpy, PROVIDER_SPIES} from "../test/test-utils";
+import {of} from "rxjs";
 
 describe('SongComponent', () => {
   let component: SongComponent;
   let fixture: ComponentFixture<SongComponent>;
 
+  let activatedRoute: SpyObj<ActivatedRoute>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SongComponent]
+      imports: [SongComponent],
+      providers: [
+        PROVIDER_SPIES.ActivatedRoute,
+      ]
     })
     .compileComponents();
-    
+
+    activatedRoute = injectSpy(ActivatedRoute);
+    // TODO on est obligé de le mettre dans beforeEach, car ça ne marche pas dans it
+    activatedRoute.params = of({
+      songName: 'Petit Papillon',
+    });
+
     fixture = TestBed.createComponent(SongComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/test/test-utils.ts
+++ src/app/test/test-utils.ts
@@ -1,8 +1,13 @@
+import {ActivatedRoute} from "@angular/router";
+import {ProviderToken} from "@angular/core";
+import {TestBed} from "@angular/core/testing";
+import createSpyObj = jasmine.createSpyObj;
+import SpyObj = jasmine.SpyObj;
 
 // Source : https://stackoverflow.com/a/57331494/1655155
 export function getKarmaFile(filePath: string): Promise<Blob> {
   return new Promise((resolve) => {
-    const request: XMLHttpRequest = createGetKarmaFileRequest(filePath );
+    const request: XMLHttpRequest = createGetKarmaFileRequest(filePath);
 
     request.onload = async r => {
       const blob = new Blob([request.response]);
@@ -19,4 +24,17 @@ function createGetKarmaFileRequest(filePath: string): XMLHttpRequest {
   request.open('GET', 'base/' + filePath, true);
   request.responseType = 'arraybuffer'; // maybe also 'text'
   return request;
+}
+
+export const PROVIDER_SPIES = {
+  ActivatedRoute: {
+    provide: ActivatedRoute,
+    useValue: createSpyObj<ActivatedRoute>('ActivatedRoute', [
+      'params',
+    ] as (keyof ActivatedRoute)[]),
+  },
+}
+
+export function injectSpy<T>(token: ProviderToken<T>): SpyObj<T> {
+  return TestBed.inject(token) as SpyObj<T>;
 }

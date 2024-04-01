647552aa52cf3a190988cb40415582f726c68229
Bludwarf <bludwarf@gmail.com>
2024-04-01 11:00:58 +0200

    fix: Affichage mobile pour répéter un morceau
    Closes: #17

--- src/app/rehearsal/mobile/mobile-rehearsal-a/mobile-rehearsal-a.component.spec.ts
+++ src/app/rehearsal/mobile/mobile-rehearsal-a/mobile-rehearsal-a.component.spec.ts
@@ -1,16 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MobileRehearsalAComponent } from './mobile-rehearsal-a.component';
+import {injectSpy, PROVIDER_SPIES} from "../../../test/test-utils";
+import {ActivatedRoute} from "@angular/router";
+import SpyObj = jasmine.SpyObj;
+import {of} from "rxjs";
 
 describe('MobileRehearsalAComponent', () => {
   let component: MobileRehearsalAComponent;
   let fixture: ComponentFixture<MobileRehearsalAComponent>;
+  let activatedRoute: SpyObj<ActivatedRoute>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MobileRehearsalAComponent]
+      imports: [MobileRehearsalAComponent],
+      providers: [
+        PROVIDER_SPIES.ActivatedRoute,
+      ]
     })
     .compileComponents();
+
+    activatedRoute = injectSpy(ActivatedRoute);
+    activatedRoute.params = of({
+      songName: 'Petit Papillon',
+    });
     
     fixture = TestBed.createComponent(MobileRehearsalAComponent);
     component = fixture.componentInstance;

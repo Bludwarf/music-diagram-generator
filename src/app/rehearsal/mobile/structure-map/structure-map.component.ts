4222f50be9d639a3cbdd8161c7d5c45e1299900d
Bludwarf <bludwarf@gmail.com>
2024-04-02 12:56:17 +0200

    feat: Composant structure-map
    Closes: #23

---
+++ src/app/rehearsal/mobile/structure-map/structure-map.component.ts
@@ -0,0 +1,24 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Structure} from "../../../structure/structure";
+import {NgForOf} from "@angular/common";
+import {PatternInStructure} from "../../../structure/pattern/pattern-in-structure";
+
+@Component({
+  selector: 'app-structure-map',
+  standalone: true,
+  imports: [
+    NgForOf
+  ],
+  templateUrl: './structure-map.component.html',
+  styleUrl: './structure-map.component.scss'
+})
+export class StructureMapComponent {
+  @Input() structure!: Structure;
+  @Input() timecode?: string;
+  @Input() currentPatternInStructure: PatternInStructure | undefined;
+  @Output() clickPatternInStructure = new EventEmitter<PatternInStructure>();
+
+  onClickPatternInStructure(patternInStructure: PatternInStructure) {
+    this.clickPatternInStructure.emit(patternInStructure);
+  }
+}
